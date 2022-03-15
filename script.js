b5fc49b4346e7fbb5386f53a3432b7b300bc6b46
Luthando Mtengwane <luthandolindokuhle17@gmail.com>
2022-03-16 01:58:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Challenge 1: Your age in days
+
+function ageInDays() {
+    let birthYear = prompt('What year were you born?');
+    let ageInDayss = (2018 - birthYear) * 365;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1)
+    
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+
